b74199db16c2fcea9233fdbef8c3d3e681af9cd1
dengzhiqiang <1445124133@qq.com>
2017-12-17 21:15:30 +0800

    '提示框'

---
+++ src/js/layerMobile.js
@@ -0,0 +1,17 @@
+;(function (win) {
+
+    var tpl = {
+        tip: function () {
+            /**
+             * 没有按钮
+             * 没有标题
+             * 仅仅显示内容会自动关闭的提示框
+             *
+             */
+
+            var str ="<div class='layerMobie layerMobie-tip'></div>";
+        }
+    }
+
+
+})(window);
